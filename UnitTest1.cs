b34b84b4594cee6f8f5478dc6be10bf7e6fb412c
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-09-22 15:22:21 +0530

    [ASHIQ] added : UnitTest1.cs

---
+++ UnitTest1.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using RegexDemo058;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Addition add = null;
+        [TestInitialize]
+        
+        public void SetUp()
+        {
+            add = new Addition();
+        }
+
+        [TestMethod]
+        public void TestSum()
+        {
+            ///AAA methodology
+
+            /// Arrange
+            int first = 12;
+            int second = 34;
+            int expected = 156;
+
+            //Act
+            int actualResult=add.Sum(first, second);
+
+            //Assert
+            Assert.AreEqual(expected, actualResult);
+        }
+
+        [TestMethod]
+        public void TestSum1()
+        {
+            ///AAA methodology
+
+            /// Arrange
+            int first = 12;
+            int second = 34;
+            int expected = 156;
+
+            //Act
+            int actualResult = add.Sum(first, second);
+
+            //Assert
+            Assert.AreEqual(expected, actualResult);
+        }
+    }
+}
+
